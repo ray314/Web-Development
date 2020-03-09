eaa5dbd0bc921cba6aef72e230f8fedfe28496ce
jusyeung <yeungj1602@gmail.com>
2020-03-09 16:35:16 +1300

    added php code, retested with french days

---
+++ Lab2/daysarray.php
@@ -0,0 +1,5 @@
+<?php
+    $days = array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+    echo "The Days of the week in French are: " . $days[0] . ", " . $days[1] . ", " . $days[2] . ", " . $days[3] . ", "
+    . $days[4] . ", " . $days[5] . ", " . $days[6] . ".";
+?>
