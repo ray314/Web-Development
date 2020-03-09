8af9be689dce4b214092ccb337782a4b01d9946b
jusyeung <yeungj1602@gmail.com>
2020-03-09 17:38:09 +1300

    added factorial task

---
+++ Lab2/factorial.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html" charset="UTF-8">
+</head>
+
+<body>
+
+<?php
+
+include 'mathfunctions.php';
+
+$number = $_GET['number'];
+
+if ($number >= 0)
+{
+    echo "Factorial of " . $number . " is " . factorial($number);
+}
+else {
+    echo "This is not a valid number";
+}
+
+?>
+
+</body>
+
+</html>
