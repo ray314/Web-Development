7e7f9c8ee51adc8386ffec3a8b6be8d7e5014e38
jusyeung <yeungj1602@gmail.com>
2020-03-09 18:32:07 +1300

    Added meta to php files, added leap year task

--- Lab2/daysarray.php
+++ Lab2/daysarray.php
@@ -1,5 +1,15 @@
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html" charset="UTF-8">
+</head>
+
+<body>
 <?php
     $days = array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
     echo "The Days of the week in French are: " . $days[0] . ", " . $days[1] . ", " . $days[2] . ", " . $days[3] . ", "
     . $days[4] . ", " . $days[5] . ", " . $days[6] . ".";
 ?>
+</body>
+
+</html>
---
+++ Lab2/leapyear.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html" charset="UTF-8">
+</head>
+
+<body>
+    <h1>Lab 2 - Leap Year</h1>
+    <?php
+
+    function is_leapyear($year) 
+    {
+        if (!is_numeric($year))
+        {
+            echo "This is not a valid year or number";
+            return false;
+        }
+
+        if ($year % 4 == 0) // 0 = divisible by 4
+        {
+            // Two situations:
+            // 1. Not divisible by 100, it is a leap year
+            // 2. Divisible by 100 and 400, it is a leap year
+            if (($year % 100 != 0) || ($year % 100 == 0) && ($year %400 == 0)) // Not divisible by 100 or divisible by 100 and 400
+            {
+                echo $year . " is a leap year";
+                return true;
+            }
+            else
+            {
+                echo $year . " is not a leap year";
+                return false;
+            }
+        }
+        else
+        {
+            echo $year . " is not a leap year";
+            return false;
+        }
+    }
+    $test = is_leapyear($_GET['year']);
+    echo "\n";
+    echo $test;
+    ?>
+</body>
+
+</html>
