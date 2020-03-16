b068379ac2fde450122ae214dfc95e95968a4957
jusyeung <yeungj1602@gmail.com>
2020-03-16 16:11:46 +1300

    Palindrome done

---
+++ Lab3/perfectpalindrome.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+        <title>Using palindrome functions</title>
+    </head>
+
+    <body>
+        <h1>Lab 03 Task 2 - Perfect Palindrome</h1>
+        <?php
+            if (!$_POST['word'] == "") {
+                $word = $_POST['word']; // Obtain info
+                $word = strtolower($word); // Convert to lowercase
+                $reversedWord = strrev($word); // Reverse word
+                // Compare original with reversed, 0 = equal
+                if (strcmp($word, $reversedWord) == 0) {
+                    echo "<p>The text you entered '", $word, "' is a perfect palindrome!</p>";
+                } else {
+                    echo "<p>The text you entered '", $word, "' is not a palindrome</p>";
+                }
+            } else {
+                echo "<p>Please enter a word.</p>";
+            }
+        ?>
+    </body>
+</html>
