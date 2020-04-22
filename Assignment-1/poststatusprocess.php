67d11d7eb6391af5539c61b9757c6fb56485e934
ray314 <yeungj1602@gmail.com>
2020-04-23 11:44:22 +1200

    Update poststatusprocess.php

--- Assignment-1/poststatusprocess.php
+++ Assignment-1/poststatusprocess.php
@@ -21,7 +21,7 @@
 
             $permissions = "";
             if (!isset($checkbox)) {
-                echo "No permissions were selected";
+                echo "<br>No permissions were selected";
             } else {
                 for ($i = 0; $i < count($checkbox); $i++) {
                     $permissions .= $checkbox[$i]; // Format the permissions
@@ -35,7 +35,7 @@
             $conn = new mysqli($servername, $username, $password, $dbname);
 
             // Check connection
-            if ($conn->connect_error) {
+            if (@$conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
             echo "Connected successfully";
