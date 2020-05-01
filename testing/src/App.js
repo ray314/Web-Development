3ecc70bd2d0b8d89c025cd80a75bb1a73e61d8d3
ray314 <yeungj1602@gmail.com>
2020-05-02 08:05:59 +1200

    Update App.js

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Game from "./components/Game";
 
 function App() {
   return (
     <div className="App">
+      <Game />
     </div>
   );
 }
