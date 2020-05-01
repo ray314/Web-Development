fc700f2d19f04ec1b544c7ecd28fdbc24a5312c2
ray314 <yeungj1602@gmail.com>
2020-05-02 08:03:40 +1200

    updated

---
+++ testing/src/components/Game.js
@@ -0,0 +1,15 @@
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{}</div>
+                    <ol>{}</ol>
+                </div>
+            </div>
+        );
+    }
+}
