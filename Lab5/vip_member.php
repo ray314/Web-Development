09dd3004ba17b62958c356f6d1140e305af1e2e2
ray314 <yeungj1602@gmail.com>
2020-05-04 13:32:17 +1200

    updated

---
+++ Lab5/vip_member.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <title>Using file functions</title> 
+    </head>
+
+    <div class="container">
+        <body>
+            <h1>Web Development - Lab05</h1>
+            <br><br>
+            <a href="member_add_form.php">Add New Member Form</a>
+            <br>
+            <a href="member_display.php">Display All Members Page</a>
+            <br>
+            <a href="member_search.php">Search Member Page</a>
+        </body>
+        
+    </div>
+</html>
