a656be39fdf27ae9b2482b5e22f4071910a0ed4e
ray314 <yeungj1602@gmail.com>
2020-05-19 10:32:55 +1200

    removed sleep

--- lab07/data.php
+++ lab07/data.php
@@ -1,10 +1,9 @@
 <!--file data.php -->
 <?php
 	// get name and password passed from client
-	$name = $_GET['name'];
-	$pwd = $_GET['pwd'];
-	// sleep for 10 seconds to slow server response down
-	sleep(10);
+	$name = $_POST['name'];
+	$pwd = $_POST['pwd'];
+	
 	// write back the password concatenated to end of the name
 	ECHO ($name." : ".$pwd)
 ?>
