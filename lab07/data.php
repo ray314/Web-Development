054b2e17cfc9cb138a6feef6386f261567dba0bc
ray314 <yeungj1602@gmail.com>
2020-05-19 12:55:18 +1200

    updated

--- lab07/data.php
+++ lab07/data.php
@@ -3,7 +3,29 @@
 	// get name and password passed from client
 	$name = $_POST['name'];
 	$pwd = $_POST['pwd'];
+
+	// Get the credentials
+	require_once("../../config/info.php");
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	$sql = "SELECT email FROM credentials WHERE username = '$name' AND password = '$pwd';";
 	
-	// write back the password concatenated to end of the name
-	ECHO ($name." : ".$pwd)
+	// query the database
+	$result = $conn->query($sql);
+
+	// Fetch the email
+	$row = $result->fetch_assoc();
+	if(isset($row)) {
+		echo ("<p>Email: ".$row['email'] . "</p>");
+	} else {
+		// Check if username exists
+		$sql = "SELECT username FROM credentials WHERE username = '$name';";
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+		if (isset($row)) {
+			echo "Your password is incorrect";
+		} else {
+			echo ("This username does not exist.");
+		}
+	}
 ?>
--- lab07/simpleajax.js
+++ lab07/simpleajax.js
@@ -1,17 +1,17 @@
 // file simpleajax.js
 var xhr = createRequest();
-function getData(dataSource, divID, aName, aPwd)  {
+function getData(dataSource, divID, aName, aPwd, aEmail)  {
     if(xhr) {
 	    var place = document.getElementById(divID);
 	    var url = dataSource;
 		xhr.open("POST", url, true);
 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	    xhr.onreadystatechange = function() {
-		    alert(xhr.readyState);
 			if (xhr.readyState == 4 && xhr.status == 200) {
 			place.innerHTML = xhr.responseText;
 		    } // end if
-	    } // end anonymous call-back function
-	    xhr.send("name="+aName+"&pwd="+aPwd);
+		} // end anonymous call-back function
+		//aPwd = encodeURIComponent(aPwd);
+	    xhr.send("name="+aName+"&pwd="+aPwd+"&email="+aEmail);
 	} // end if
 } // end function getData()
