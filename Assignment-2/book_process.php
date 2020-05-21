b94a90dd40a3513ccd82491e3ad50ba004fb90ef
ray314 <yeungj1602@gmail.com>
2020-05-21 15:08:29 +1200

    updated

--- Assignment-2/book_process.php
+++ Assignment-2/book_process.php
@@ -1,4 +1,16 @@
 <?php
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$pickup = $_POST['pickup'];
+$dest = $_POST['dest'];
+$datetime = $_POST['datetime'];
+// Get the credentials
+require_once("../../config/info.php");
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    echo "Error connecting to the database. Error number: " . $conn->connect_errno;
+    die();
+}
 
 ?>
