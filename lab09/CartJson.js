79eb82e69805bf669f7805d2036bcba8181658f5
ray314 <yeungj1602@gmail.com>
2020-06-03 14:30:46 +1200

    update

--- lab09/CartJson.js
+++ lab09/CartJson.js
@@ -1,40 +1,30 @@
-var xHRObject = new XMLHttpRequest();
-
-
-function getData()
-{
-    if ((xHRObject.readyState == 4) &&(xHRObject.status == 200))
-    {
-        // var obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}'); 
-		
-		var cart = document.getElementById("cart");
-		
-		var serverResponse;
-		if (xHRObject.responseText != "") {
-            serverResponse= JSON.parse(xHRObject.responseText);
-        } 
-		else {
-            serverResponse=null;
-        } 
-		
-		if (serverResponse != null){
-			
-			var keys = Object.keys(serverResponse);
-			//cart.innerHTML = "";
-            //cart.innerHTML += "test " +keys[0];
-            cart.innerHTML = "test " + serverResponse[keys[0]] + " " + "<a href='#' onclick='AddRemoveItem(\"Remove\");'>Remove Item</a>";
-        
-        }
-        else{  cart.innerHTML = ""; }
-    }
-}
-
 function AddRemoveItem(action)
 {
+    var xHRObject = new XMLHttpRequest();
     var book  = document.getElementById("book").innerHTML;
+    var isbn = document.getElementById("ISBN").innerHTML;
+    var cost = document.getElementById("price").value;
            
-	xHRObject.open("GET", "test.php?action="+action+"&book="+book+"&value="+Number(new Date), true);
-    xHRObject.onreadystatechange = getData;
+	xHRObject.open("GET", "test.php?action="+action+"&book="+book+"&isbn="+isbn+"&cost="+cost, true);
+    xHRObject.onreadystatechange = function () {
+        if ((this.readyState == 4) && (this.status == 200)) {
+		    var cart = document.getElementById("cart");
+		    if (this.responseText != "") {
+                console.log(this.responseText);
+                var myObj = JSON.parse(this.responseText);
+                
+                // Display cart if value is greater than 0
+                if (myObj.value > 0) {
+                    cart.innerHTML = " " + myObj.book + " " + "<a href='#' onclick='AddRemoveItem(\"Remove\");'>Remove Item</a>";
+                    cart.innerHTML += " " + myObj.value + "\n";
+                    cart.innerHTML += "Total: $"+myObj.totalCost;
+                } 
+            } else {
+                // Else clear the cart
+                cart.innerHTML = "";
+            } 
+        }
+    };
     xHRObject.send();   
 }
 
--- lab09/test.php
+++ lab09/test.php
@@ -1,42 +1,46 @@
 <?php
-
 session_start();
+//error_reporting(E_ERROR | E_PARSE);
+// If cart session variable doesn't exist then create a new one
+if (!isset($_SESSION["Cart"])) {
+    // 
+    $cart = new stdClass();
+    $_SESSION["Cart"] = $cart;
+} else {
+    $cart = $_SESSION["Cart"];
+}
+// Use an object to store data
+$cart->book = $_GET["book"];
+$cart->isbn = $_GET["isbn"];
+$action = $_GET["action"]; // Add or Remove
+$cost = $_GET["cost"]; // Cost of book
 
-$newitem = $_GET["book"];
-$action = $_GET["action"];
 
-		if (array_key_exists("Cart", $_SESSION))
-        {
-            $cart = $_SESSION["Cart"];
-            if ($action == "Add")
-            {
-                if ($cart[$newitem] != "")
-                {  
-                    $value = $cart[$newitem] + 1;
-                    $cart[$newitem] = $value;
-                }
-                else
-                {
-                    $cart[$newitem] = "1";
-                }
-            }
-            else
-            {
-                $cart[$newitem]= "";
-            }
-        }
-        else
-        {
-            $cart[$newitem] = "1";
-        }
+// If value set then do action
+if (isset($cart->value))
+{
+    if ($action == "Add")
+    {
+        $cart->value++;
+        $cart->totalCost += $cost;
+    }
+    else if ($cart->value > 0)
+    {
+        $cart->value--;
+        $cart->totalCost -= $cost;
+    }
+}
+else
+{
+    $cart->value = 1;
+}
 
-        $_SESSION["Cart"] = $cart;
+$_SESSION["Cart"] = $cart;
 
-        
-
-        if ($cart[$newitem] != "") 
-			echo json_encode($cart, JSON_PRETTY_PRINT);
-		
-		// echo json_encode($cart, JSON_PRETTY_PRINT);
+if ($cart->value != 0) {
+    echo json_encode($cart);
+} else {
+    echo "";
+}
 		
 ?>
